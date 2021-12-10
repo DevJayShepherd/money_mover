f642ebe9aab11319454383ef9ba7704a2646b3b9
Jay Shepherd <69314244+DevJayShepherd@users.noreply.github.com>
2021-12-10 12:23:52 +0000

    Minor fixes (#9)

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from webapp.config import settings
 
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__, template_folder='webapp/templates')
 app.config['SQLALCHEMY_DATABASE_URI'] = settings.SQLALCHEMY_DATABASE_URI
 app.secret_key = settings.SECRET_KEY
 db = SQLAlchemy(app)
